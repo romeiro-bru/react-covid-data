159912a2acc4d20521e3fbc1f80c7e869996016b
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-10 19:32:09 -0300

    Fetch data from app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Cards } from './components/Cards/Cards';
 import { CountrySelector } from './components/CountrySelector/CountrySelector';
+import axios from 'axios';
+
+const url = "https://covid19.mathdro.id/api";
 
 function App() {
+  const [getData, setGetData] = useState([])
+
+  useEffect(() => {
+      async function fetchData() {
+          const response = await axios.get(url)            
+          const allData = {
+              confirmed: response.data.confirmed,
+              recovered: response.data.recovered,
+              deaths: response.data.deaths,
+              lastUpdate: response.data.lastUpdate
+          }
+          setGetData(allData)
+      }
+      return fetchData()
+  }, [])
+  console.log(getData)
+
   return (
     <div className="App">
      <h1>Covid-19 tracker!</h1>
