544f0b29077abaf359e3c6cc119b30c42e9d8358
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-10 18:31:34 -0300

    Fetch data

--- src/components/CountrySelector/CountrySelector.jsx
+++ src/components/CountrySelector/CountrySelector.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const url = "https://covid19.mathdro.id/api";
+
 export function CountrySelector() {
+    const [getData, setGetData] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get(url)
+            setGetData(response.data)
+        }
+        return fetchData()
+    }, [])
+    console.log(getData)
+
     return(
         <h1>Country selector</h1>
     )
-}
+} 
