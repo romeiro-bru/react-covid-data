264551e3231e9ead3eb06a67cefa7fe445587007
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-10 11:43:48 -0300

    Import components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Cards } from './components/Cards/Cards';
+import { CountrySelector } from './components/CountrySelector/CountrySelector';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello tracker!</h1>
+     <Cards />
+     <CountrySelector />
     </div>
   );
 }
