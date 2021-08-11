74f059a0160ed36c10a080cab1171b564683a76c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-10 22:55:23 -0300

    Add components from material-ui

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,8 +5,26 @@ import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 export function Cards({ data }) {
     console.log(2, data)
     return(
-        <section>
+        <section className="cards-container">
+            <Grid container justify='center' spacing={2}  >
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">Data</Typography>
+                        <Typography color="textSecondary">Data</Typography>
+                        <Typography variant="body2">Number of cases</Typography>
+                    </CardContent>
+                </Grid>
 
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">Data</Typography>
+                        <Typography color="textSecondary">Data</Typography>
+                        <Typography variant="body2">Number of recovered</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
         </section>
     )
 }
