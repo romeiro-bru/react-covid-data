969ac364e2fbff8ba6eeb1745ea9c53638737236
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-11 14:08:22 -0300

    Add verification

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './style.scss';
 import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 
-export function Cards({ data }) {
-    console.log(2, data)
+export function Cards({ data: {confirmed, recovered, deaths, lastUpdate}}) {
+    if(!confirmed) {
+        return 'loading..'
+    }
+    console.log(2, recovered)
     return(
         <section className="cards-container">
             <Grid container justify='center' spacing={2}  >
@@ -22,6 +25,16 @@ export function Cards({ data }) {
                         <Typography variant="h5">Data</Typography>
                         <Typography color="textSecondary">Data</Typography>
                         <Typography variant="body2">Number of recovered</Typography>
+                    </CardContent>
+                </Grid>
+
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">Data</Typography>
+                        <Typography color="textSecondary">Data</Typography>
+                        <Typography variant="body2">Number of </Typography>
                     </CardContent>
                 </Grid>
             </Grid>
