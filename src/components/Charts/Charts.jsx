c4398e1388499a262836ac7c019ec0aaa144e86c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-17 18:35:31 -0300

    Create charts, Import charts

---
+++ src/components/Charts/Charts.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style.css';
+import {url} from '../../App';
+
+export function Charts() {
+    const [dailyData, setDailyData] = useState({})
+
+    useEffect(() => {
+        async function fetchDailyData() {
+            const dailyResponse = await axios.get(`${url}/daily`)  
+            setDailyData(dailyResponse)         
+            console.log(1, dailyResponse)
+        }
+        return fetchDailyData()
+    })
+
+    return(
+        <h1>Charts</h1>
+    )
+} 
