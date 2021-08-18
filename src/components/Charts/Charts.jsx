4dee1a97156bb5cc38cdaa72cc847d7de014431a
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-18 18:30:24 -0300

    Add react charts, Add Line chart

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,20 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './style.css';
 import {url} from '../../App';
+import { Line } from 'react-chartjs-2';
+
+import './style.css';
 
 export function Charts() {
-    const [dailyData, setDailyData] = useState({})
+    const [dailyData, setDailyData] = useState([])
 
     useEffect(() => {
         async function fetchDailyData() {
             const dailyResponse = await axios.get(`${url}/daily`)  
-            setDailyData(dailyResponse)         
+            setDailyData(dailyResponse.data)         
+            console.log(dailyData)
         }
         return fetchDailyData()
-    })
+    }, [])
 
-    return(
-        <h1>Charts</h1>
+    const lineChart = (
+        dailyData.length !== 0 ? (
+            <Line data={{
+                labels: dailyData.map((item) => {
+                    return (item.reportDate)
+                }),
+                datasets: [{
+                    data:dailyData.map((item) => {
+                        return (item.totalConfirmed)
+                    }),
+                    label: 'Infected',
+                    borderColor: '#333ff',
+                    fill: true
+                }, {
+                    data:dailyData.map((item) => {
+                        return (item.deaths.total)
+                    }),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                    fill: true
+                }]
+            }} />
+        ) : null
+    )
+
+    return (
+        <section className="chart-container">
+            {lineChart}
+        </section>
     )
 } 
