456b3015fe429ecaee9ea02c5b17f66c25e15037
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-08-19 12:12:51 -0300

    Import components from materialui, Fetch countries

--- src/components/CountrySelector/CountrySelector.jsx
+++ src/components/CountrySelector/CountrySelector.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import axios from 'axios';
+import {url} from '../../App';
 import './style.css';
 
-export function CountrySelector({ data }) {
+export function CountrySelector() {
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        async function fetchCountries() {
+            const CountriesResponse = await axios.get(`${url}/countries`)  
+            setCountries(CountriesResponse.data.countries)         
+        }
+        return fetchCountries()
+    }, [])
 
     return(
-        <div className='container'>
-         <h1>Country selector</h1>
-        </div>
+        <FormControl className="country-form">
+            <Select>
+                <option value="">Global</option>
+                {countries.map((country, index) => {
+                    return (
+                        <option key={index} value={country.name}>
+                            {country.name}
+                        </option>
+                    )
+                })}
+            </Select>
+        </FormControl>
     )
 } 
